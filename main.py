d4c35b1df4563772f45578362a87580a602dcda4
krzysztofHyzyk <krszysztofhgithhub@gmail.com>
2022-11-30 20:45:50 +0100

    dodane mazanie zawartości inputu

--- main.py
+++ main.py
@@ -5,29 +5,39 @@ from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
+def search(driver, text):
+    el = driver.find_element(By.XPATH, "//input[@type='search']")
+    # create action chain object
+    action = ActionChains(driver)
+    # click the item
+    action.click(on_element=el)
+    c = '\u0001';
+    action.send_keys(str(c));
+    action.perform()
+    action.send_keys(Keys.BACKSPACE)
+    action.perform()
+    # send keys
+    action.send_keys(f"indeks glikemiczny {text}")
+    action.send_keys(Keys.RETURN);
+    # perform the operation
+    action.perform()
+    time.sleep(2)
+    el = driver.find_element(By.XPATH, "//div[@class='b_focusTextMedium']")
+    return el.text
+
 chromedriver = r"chromedriver.exe"
 brave = 'C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe'
 option = uc.ChromeOptions()
-option.add_argument("--headless")
+#option.add_argument("--headless")
 option.binary_location = brave
 driver = uc.Chrome(driver_executable_path=chromedriver, options=option)
 
 driver.get('https://www.bing.com/')
-el = driver.find_element(By.XPATH,"//input[@type='search']")
-# create action chain object
-action = ActionChains(driver)
-# click the item
-action.click(on_element=el)
-# send keys
-action.send_keys("indeks glikemiczny jabłko")
-action.send_keys(Keys.RETURN);
-# perform the operation
-action.perform()
-el = driver.find_element(By.XPATH,"//div[@class='b_focusTextMedium']")
-print(el.text)
+print(search(driver,"jabłko"))
+print(search(driver,"ziemniak"))
 
 
-
+input()
 
 
 # driver.get("https://google.pl")
